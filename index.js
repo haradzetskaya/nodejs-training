a8c2cfdd179572bda55b1605e393283c8aee7ca8
haradzetskaya <h.haradzetskaya@godeltech.com>
2024-03-10 19:44:00 +0100

    PROXY-2: count asteroids

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-const hello = () => 'Hello world';
+const https = require('https');
+const { isWeekend, isMonday, isFriday, previousMonday, nextFriday, previousFriday, addWeeks, format } = require('date-fns')
+const API_KEY = '4k6hVqvFRyEg2dmxoK7T7kTCP1EJdHiVf8PgDM5W';
 
-console.log(hello());
+const now = new Date();
+const weekend = isWeekend(now);
+const formatDate = 'yyyy-MM-dd';
+let start;
+let end;
+
+if (weekend) {
+    start = previousMonday(now);
+    end = previousFriday(now);
+} else {
+    const previousWeekDay = addWeeks(now, -1);
+    start = isMonday(now) ? previousWeekDay : previousMonday(previousWeekDay);
+    end = isFriday(now) ? previousWeekDay : nextFriday(previousWeekDay);
+}
+
+const startDate = format(start.toLocaleDateString(), formatDate);
+const endDate = format(end.toLocaleDateString(), formatDate);
+
+https.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + startDate + '&end_date=' + endDate + '&api_key=' + API_KEY,
+    (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            let parsedJson = JSON.parse(data);
+            console.log(parsedJson["element_count"]);
+        });
+    });
