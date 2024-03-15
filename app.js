736a0c2a9b68446f2a19a3a6eacd4ead2f509217
haradzetskaya <h.haradzetskaya@godeltech.com>
2024-03-15 18:27:27 +0100

    PROXY-5: add proxy call

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { isWeekend, isMonday, isFriday, previousMonday, nextFriday, previousFriday, addWeeks, format } = require('date-fns')
 dotenv.config();
 let app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log('Server running on ' + process.env.PORT);
+const now = new Date();
+const weekend = isWeekend(now);
+const formatDate = 'yyyy-MM-dd';
+let start;
+let end;
+
+if (weekend) {
+    start = previousMonday(now);
+    end = previousFriday(now);
+} else {
+    const previousWeekDay = addWeeks(now, -1);
+    start = isMonday(now) ? previousWeekDay : previousMonday(previousWeekDay);
+    end = isFriday(now) ? previousWeekDay : nextFriday(previousWeekDay);
+}
+
+const startDate = format(start.toLocaleDateString(), formatDate);
+const endDate = format(end.toLocaleDateString(), formatDate);
+
+app.listen(process.env.PORT, (error) => {
+    error 
+        ? console.log(error)
+        : console.log('Server running on ' + process.env.PORT);
 })
+const target = process.env.URL + '?start_date=' + startDate + '&end_date=' + endDate + '&api_key=' + process.env.API_KEY;
+
+app.get('/meteors', async (req, res) => {
+
+    let resp = await fetch(target);
+    const data = await resp.json();
+
+    let datum = "Asteroids: " + data["element_count"];
+    res.send(datum);
+});
